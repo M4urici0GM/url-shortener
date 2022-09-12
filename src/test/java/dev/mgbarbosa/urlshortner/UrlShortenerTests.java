ea70ced6a9f8eacf0108a80437bd9c4767a2b48c
Mauricio Barbosa <mgbftw@gmail.com>
2022-09-12 18:31:00 -0300

    Renamed class

--- src/test/java/dev/mgbarbosa/urlshortner/UrlShortenerTests.java
+++ src/test/java/dev/mgbarbosa/urlshortner/UrlShortenerTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
-class AccessingDataMongodbApplicationTests {
+class UrlShortenerTests {
 
 	@Autowired
 	private RedisTemplate<?, ?> redisTemplate;
