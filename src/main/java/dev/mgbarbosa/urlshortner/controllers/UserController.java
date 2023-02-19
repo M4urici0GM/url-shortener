b3957694d6a63aaa2bf34827cab498db2287769c
Mauricio Gomes Barbosa <mgbftw@gmail.com>
2023-02-19 17:40:47 -0300

    removed /api from all controllers for nginx to rewrite from

--- src/main/java/dev/mgbarbosa/urlshortner/controllers/UserController.java
+++ src/main/java/dev/mgbarbosa/urlshortner/controllers/UserController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/api/v1/user")
+@RequestMapping(path = "/v1/user")
 public class UserController {
     private final UserService userService;
 
